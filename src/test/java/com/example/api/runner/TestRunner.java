8842a80186e57a5280296713e0bb2ac7c5cdbb9e
Navya <navyaaj@gmail.com>
2026-04-24 22:26:24 +0100

    changes in test runner

--- src/test/java/com/example/api/runner/TestRunner.java
+++ src/test/java/com/example/api/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions(strict = true,
         features = "src/test/resources/features",
         glue = "com.example.api.steps",
         plugin = {
